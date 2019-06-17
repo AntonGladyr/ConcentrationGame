8b7266bfdaad5590462d7a8ade5771f03b01c2a4
Anton Gladyr <anton.gladyr@gmail.com>
2019-06-17 18:57:07 +0300

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Anton Gladyr on 6/17/19.
+//  Copyright © 2019 Anton Gladyr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
